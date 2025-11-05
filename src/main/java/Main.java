0cc72a0fa7777cb0c75798af90ff344bb331fcc7
salih-sengul <salih.sengul@outlook.com>
2025-11-05 19:43:17 +0300

    Comment out developer code and update userId in tests
    Commented out the developer-related code in Main.java and added a Rectangle area calculation example. Updated the userId in ResultAnalyzer.java from '999999' to '301545' for test result reporting.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ import com.workintech.pool.Rectangle;
 public class Main {
     public static void main(String[] args) {
 
-        JuniorDeveloper juniorDeveloper1 = new JuniorDeveloper(1212,"Ahmet",25000);
+        /*JuniorDeveloper juniorDeveloper1 = new JuniorDeveloper(1212,"Ahmet",25000);
         JuniorDeveloper[] juniorDevelopers = new JuniorDeveloper[5];
         MidDeveloper[] midDevelopers = new MidDeveloper[5];
         SeniorDeveloper[] seniorDevelopers = new SeniorDeveloper[5];
@@ -21,7 +21,11 @@ public class Main {
         hrManager.addEmployee(1,juniorDeveloper1);
 
         System.out.println(hrManager.toString());
+*/
+    Rectangle rectangle =new Rectangle(10.00f,20.00f);
 
+    double area=rectangle.getArea();
 
+        System.out.println(area);
     }
 }
